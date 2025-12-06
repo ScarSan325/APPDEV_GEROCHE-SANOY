342e2292318e755a4b60ea2b1b495246d0c1cda7
mikaminikol <mecahmaynicoleg@gmail.com>
2025-12-06 14:57:03 +0800

    added missing files

---
+++ src/App copy.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Car from "./component/Car.jsx"
+import FormContainer from "./component/FormContainer.jsx"
+import AnimalComponent from "./component/Animal.jsx"
+import { Login } from '@mui/icons-material'
+import Box from "@mui/material/Box";
+import {Loginform, SignUpform} from './component/login.jsx'
+
+function App() {
+  return(
+
+    /*<div>
+      <Car/>
+      <FormContainer/>
+      <AnimalComponent/>
+    </div>
+    <>
+      <h1>Hello Mika and Scar koko</h1>
+    </>*/
+    <div>
+      <Car/>
+     <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        gap: 2,
+        mt: 4,
+      }}
+    >
+        <Loginform />
+        <SignUpform />
+    </Box>
+    </div>
+  
+  )
+}
+
+export default App
